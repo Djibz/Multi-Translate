2618c040191af87675b60950c633df1f0e5fa7ce
Erwan Brunellière <ebrunelliere@softwaymedical.fr>
2023-07-25 18:49:07 +0100

    keys

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+// Google Certificate Fingerprint:     03:8C:32:E9:73:CA:77:8D:40:B6:1A:86:05:C5:6D:74:05:FF:BF:38
+//     Google Certificate Hash (SHA-1):    038C32E973CA778D40B61A8605C56D7405FFBF38
+//     Google Certificate Hash (SHA-256):  223018D6040601BCCCC36F8DB776CB4BCAAB918953481B32CAFF1D85EF315037
+//     Facebook Key Hash:                  A4wy6XPKd41AthqGBcVtdAX/vzg=
+
 import { StatusBar } from "expo-status-bar";
 import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { TranslateCard } from "./components/TranslateCard";
