3d489d916bd8204744f730a042df53f039698291
Erwan Brunelliere <erwan.brunelliere@etudiant.univ-lr.fr>
2023-07-28 00:56:26 +0200

    API connection

--- App.js
+++ App.js
@@ -3,11 +3,23 @@
 //     Google Certificate Hash (SHA-256):  223018D6040601BCCCC36F8DB776CB4BCAAB918953481B32CAFF1D85EF315037
 //     Facebook Key Hash:                  A4wy6XPKd41AthqGBcVtdAX/vzg=
 
+// ID : 423797242227-il4co01numtjcj6tilb0vt66va6bp2qb.apps.googleusercontent.com
+
+import * as React from "react";
+
 import { StatusBar } from "expo-status-bar";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { TranslateCard } from "./components/TranslateCard";
 
-import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LANGUAGES = [
   { name: "français", code: "fr" },
@@ -16,27 +28,70 @@ const LANGUAGES = [
 ];
 
 export default function App() {
+  const [userInfo, setUserInfo] = React.useState(null);
+  const [request, response, prompAsync] = Google.useAuthRequest({
+    androidClientId:
+      "423797242227-il4co01numtjcj6tilb0vt66va6bp2qb.apps.googleusercontent.com",
+    expoClientId:
+      "423797242227-dfp9ujma0g4t0k6ru9l2n6oh7gm8i393.apps.googleusercontent.com",
+    scopes: ["https://www.googleapis.com/auth/cloud-translation"]
+  });
+
+  function okdac() {
+    prompAsync();
+    
+  }
+
+  React.useEffect(() => {
+    console.log("Google Sign In request:", request);
+    console.log("Google Sign In Response:", response);
+  }, [response, request]);
+
+  function translate(text, source, target) {
+    console.log(response.authentication.accessToken);
+    fetch("https://translation.googleapis.com/v3beta1/projects/423797242227:translateText", {
+      method: "POST",
+      headers: {
+        Authorization: 'Bearer ' + response.authentication.accessToken,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents: [
+          text
+        ],
+        sourceLanguageCode: source,
+        targetLanguageCode: target
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        return json;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <GoogleSigninButton
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={this._signIn}
-        disabled={this.state.isSigninInProgress}
-      />
       <StatusBar style="light" />
+      <Button title="Sign in with google" onPress={okdac} />
       <FlatList
         contentContainerStyle={styles.list}
         style={{ width: "100%" }}
         data={LANGUAGES}
         renderItem={(item) => <TranslateCard item={item.item} />}
       />
+      <Button title='translate' onPress={translate.bind(this, 'bonjour', 'fr', 'pt')} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
     backgroundColor: "#292b2f",
     alignItems: "center",
